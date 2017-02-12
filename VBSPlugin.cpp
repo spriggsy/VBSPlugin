389a919cb5ffe802c6395cf0b20a94bde3d9d461
spriggsy <spriggs666@hotmail.com>
2017-02-12 11:42:28 +0000

    WORKS
    using OnSimulationStep to do the reading

--- VBSPlugin.cpp
+++ VBSPlugin.cpp
@@ -55,10 +55,14 @@ ExecuteCommandType ExecuteCommand = NULL;
 
 
 
+void task1(string msg){
+
+	cout << "task1 says: " << msg << endl;
 
 
- 
 
+
+}
 
 const char *sendX(const char *input)
 {
@@ -171,6 +175,8 @@ const char *CALIBRATE(const char *input)
 const char *CONNECT(const char *input)
 {
 
+	
+
 	// Attempt to open the serial port (COM1)
 	lLastError = serial.Open(_T("COM5"), 0, 0, false);
 	if (lLastError != ERROR_SUCCESS)
@@ -199,13 +205,21 @@ const char *CONNECT(const char *input)
 	lLastError = serial.SetupReadTimeouts(CSerial::EReadTimeoutNonblocking);
 	if (lLastError != ERROR_SUCCESS)
 		cout << serial.GetLastError() << " : " << _T("Unable to set COM-port read timeout.") << endl;
+	 
+
+	//thread t1(task1, "Hello");
+	//t1.join();
 
 
+	
 	return NULL;
 }
 const char *DISCONNECT(const char *input)
 {
 
+	// Close the port again
+	serial.Close();
+	printf("Close serial port\n");
 	return NULL;
 }
 // Function that will register the ExecuteCommand function of the engine
@@ -219,9 +233,110 @@ VBSPLUGIN_EXPORT void WINAPI RegisterCommandFnc(void *executeCommandFnc)
 // deltaT is time in seconds since the last simulation step
 VBSPLUGIN_EXPORT void WINAPI OnSimulationStep(float deltaT)
 {
-  //{ Sample code:
+  //try reading com port here
+
+	if (serial.IsOpen())
+	{
+		cout << "serial is open: "<< endl;
+
+		// Wait for an event
+		lLastError = serial.WaitEvent();
+		if (lLastError != ERROR_SUCCESS){
+
+
+			printf("\n### Unable to wait for a COM-port event. %s ###\n", serial.GetLastError());
+		}
+
+		// Save event
+		const CSerial::EEvent eEvent = serial.GetEventType();
+
+		// Handle break event
+		if (eEvent & CSerial::EEventBreak)
+		{
+			printf("\n### BREAK received ###\n");
+		}
+
+		// Handle CTS event
+		if (eEvent & CSerial::EEventCTS)
+		{
+			printf("\n### Clear to send %s ###\n", serial.GetCTS() ? "on" : "off");
+		}
+
+		// Handle DSR event
+		if (eEvent & CSerial::EEventDSR)
+		{
+			printf("\n### Data set ready %s ###\n", serial.GetDSR() ? "on" : "off");
+		}
+
+		// Handle error event
+		if (eEvent & CSerial::EEventError)
+		{
+			printf("\n### ERROR: ");
+			switch (serial.GetError())
+			{
+			case CSerial::EErrorBreak:		printf("Break condition");			break;
+			case CSerial::EErrorFrame:		printf("Framing error");			break;
+			case CSerial::EErrorIOE:		printf("IO device error");			break;
+			case CSerial::EErrorMode:		printf("Unsupported mode");			break;
+			case CSerial::EErrorOverrun:	printf("Buffer overrun");			break;
+			case CSerial::EErrorRxOver:		printf("Input buffer overflow");	break;
+			case CSerial::EErrorParity:		printf("Input parity error");		break;
+			case CSerial::EErrorTxFull:		printf("Output buffer full");		break;
+			default:						printf("Unknown");					break;
+			}
+			printf(" ###\n");
+		}
+
+		// Handle ring event
+		if (eEvent & CSerial::EEventRing)
+		{
+			printf("\n### RING ###\n");
+		}
+
+		// Handle RLSD/CD event
+		if (eEvent & CSerial::EEventRLSD)
+		{
+			printf("\n### RLSD/CD %s ###\n", serial.GetRLSD() ? "on" : "off");
+		}
+
+		// Handle data receive event
+		if (eEvent & CSerial::EEventRecv)
+		{
+			// Read data, until there is nothing left
+			DWORD dwBytesRead = 0;
+			char szBuffer[101];
+			do
+			{
+				// Read data from the COM-port
+				lLastError = serial.Read(szBuffer, sizeof(szBuffer)-1, &dwBytesRead);
+				if (lLastError != ERROR_SUCCESS)
+				{
+			 
+				printf("\nUnable to read from COM-port. %s ###\n", serial.GetLastError());
+
+			}
+
+				if (dwBytesRead > 0)
+				{
+					// Finalize the data, so it is a valid string
+					szBuffer[dwBytesRead] = '\0';
+
+					// Display the data
+					printf("%s", szBuffer);
+
+					
+				}
+			} while (dwBytesRead == sizeof(szBuffer)-1);
+		}
+	}
+
+
+
+
+
+	}
   //!}
-}
+ 
 
 // This function will be executed every time the script in the engine calls the script function "pluginFunction"
 // We can be sure in this function the ExecuteCommand registering was already done.
@@ -269,108 +384,12 @@ BOOL WINAPI DllMain(HINSTANCE hDll, DWORD fdwReason, LPVOID lpvReserved)
    {
    case DLL_PROCESS_ATTACH:
    {
-
-							  /*
-							
-							  // Keep reading data, until an EOF (CTRL-Z) has been received
-							  bool fContinue = true;
-							  do
-							  {
-								  // Wait for an event
-								  lLastError = serial.WaitEvent();
-								  if (lLastError != ERROR_SUCCESS)
-									  return ::ShowError(serial.GetLastError(), _T("Unable to wait for a COM-port event."));
-
-								  // Save event
-								  const CSerial::EEvent eEvent = serial.GetEventType();
-
-								  // Handle break event
-								  if (eEvent & CSerial::EEventBreak)
-								  {
-									  printf("\n### BREAK received ###\n");
-								  }
-
-								  // Handle CTS event
-								  if (eEvent & CSerial::EEventCTS)
-								  {
-									  printf("\n### Clear to send %s ###\n", serial.GetCTS() ? "on" : "off");
-								  }
-
-								  // Handle DSR event
-								  if (eEvent & CSerial::EEventDSR)
-								  {
-									  printf("\n### Data set ready %s ###\n", serial.GetDSR() ? "on" : "off");
-								  }
-
-								  // Handle error event
-								  if (eEvent & CSerial::EEventError)
-								  {
-									  printf("\n### ERROR: ");
-									  switch (serial.GetError())
-									  {
-									  case CSerial::EErrorBreak:		printf("Break condition");			break;
-									  case CSerial::EErrorFrame:		printf("Framing error");			break;
-									  case CSerial::EErrorIOE:		printf("IO device error");			break;
-									  case CSerial::EErrorMode:		printf("Unsupported mode");			break;
-									  case CSerial::EErrorOverrun:	printf("Buffer overrun");			break;
-									  case CSerial::EErrorRxOver:		printf("Input buffer overflow");	break;
-									  case CSerial::EErrorParity:		printf("Input parity error");		break;
-									  case CSerial::EErrorTxFull:		printf("Output buffer full");		break;
-									  default:						printf("Unknown");					break;
-									  }
-									  printf(" ###\n");
-								  }
-
-								  // Handle ring event
-								  if (eEvent & CSerial::EEventRing)
-								  {
-									  printf("\n### RING ###\n");
-								  }
-
-								  // Handle RLSD/CD event
-								  if (eEvent & CSerial::EEventRLSD)
-								  {
-									  printf("\n### RLSD/CD %s ###\n", serial.GetRLSD() ? "on" : "off");
-								  }
-
-								  // Handle data receive event
-								  if (eEvent & CSerial::EEventRecv)
-								  {
-									  // Read data, until there is nothing left
-									  DWORD dwBytesRead = 0;
-									  char szBuffer[101];
-									  do
-									  {
-										  // Read data from the COM-port
-										  lLastError = serial.Read(szBuffer, sizeof(szBuffer)-1, &dwBytesRead);
-										  if (lLastError != ERROR_SUCCESS)
-											  return ::ShowError(serial.GetLastError(), _T("Unable to read from COM-port."));
-
-										  if (dwBytesRead > 0)
-										  {
-											  // Finalize the data, so it is a valid string
-											  szBuffer[dwBytesRead] = '\0';
-
-											  // Display the data
-											  printf("%s", szBuffer);
-
-											  // Check if EOF (CTRL+'[') has been specified
-											  if (strchr(szBuffer, EOF_Char))
-												  fContinue = false;
-										  }
-									  } while (dwBytesRead == sizeof(szBuffer)-1);
-								  }
-							  } while (fContinue);
-
-							  // Close the port again
-							  serial.Close();
-
-
-
-							  */
+ 
 							  AllocConsole();
 							  freopen("CONOUT$", "w", stdout);
 
+							  
+							 
 							
 
    }
